4f2047e01069854b1f281ab9bb923c5749412855
Australis <htmlprogrammingbn@gmail.com>
2021-11-04 00:00:45 +0500

    added view city prompt!

--- core/framework/ui/page_handler.js
+++ core/framework/ui/page_handler.js
@@ -1,12 +1,14 @@
 module.exports = {
   pageHandler: function (arg0_input) {
     //Convert from parameters
+    var arg = arg0_input.split(" ");
     var input = arg0_input;
 
     //Declare local instance variables
     var game_obj = getGameObject(interaction.user);
     var user_id = interaction.user.id;
 
+    //Economy page handler
     if (economy_pages.includes(game_obj.page)) {
       switch (input) {
         case "inventory":
@@ -22,6 +24,27 @@ module.exports = {
           });
 
           break;
+      }
+    }
+
+    //Cities page handler
+    if (game_obj.page == "cities_list") {
+      if (arg[0] == "view") {
+        if (arg.length > 1) {
+          //Process city_name by removing initial argument and print city
+          var city_name = input.replace("view", "").trim();
+          printCity(game_obj.user, city_name);
+        } else {
+          visualPrompt(game_obj.alert_embed, user_id, {
+            title: "View A City:",
+            prompts: [
+              [`What is the name of the city you would like to view?`, "string"]
+            ]
+          },
+          function (arg) {
+            printCity(game_obj.user, arg[0]);
+          });
+        }
       }
     }
   }
