4920eaba7cd544798f6d8945775abfa002419b9c
Australis <htmlprogrammingbn@gmail.com>
2021-11-07 21:29:32 +0500

    added getBuilding() func!

--- core/framework/data/buildings_framework.js
+++ core/framework/data/buildings_framework.js
@@ -1,5 +1,137 @@
 module.exports = {
   /*
+    getBuilding() - Returns back a building object/key based on options
+    options: {
+      return_key: true/false
+    }
+  */
+  getBuilding: function (arg0_name, arg1_options) {
+    //Convert from parameters
+    var building_name = arg0_name.toLowerCase();
+
+    //Initialise options
+    var options = (arg1_options) ? arg1_options : {};
+
+    //Declare local instance variables
+    var all_building_categories = Object.keys(config.buildings);
+    var building_exists = [false, ""];
+
+    //Iterate over all building categories and their respective buildings - debug key
+    for (var i = 0; i < all_building_categories.length; i++) {
+      var local_building_category = config.buildings[all_building_categories[i]];
+
+      if (local_building_category[building_name])
+        building_exists = [true, (!options.return_key) ? local_building_category[building_name] : building_name];
+    }
+
+    //If the building cannot be found verbatim, start a soft-hard search by parsed strings; aliases
+    if (!building_exists[0]) {
+      //Soft match first
+      for (var i = 0; i < all_building_categories.length; i++) {
+        var local_building_category = config.buildings[all_building_categories[i]];
+        var local_buildings = Object.keys(local_building_category);
+
+        for (var x = 0; x < local_buildings.length; x++) {
+          if (!["icon", "name", "order"].includes(local_buildings[x])) {
+            var local_building = local_building_category[local_buildings[x]];
+
+            if (local_building.name)
+              if (local_building.name.toLowerCase().indexOf(building_name) != -1)
+                building_exists = [true, (!options.return_key) ? local_building : local_buildings[x]];
+          }
+        }
+      }
+
+      //Hard match second
+      for (var i = 0; i < all_building_categories.length; i++) {
+        var local_building_category = config.buildings[all_building_categories[i]];
+        var local_buildings = Object.keys(local_building_category);
+
+        for (var x = 0; x < local_buildings.length; x++) {
+          if (!["icon", "name", "order"].includes(local_buildings[x])) {
+            var local_building = local_building_category[local_buildings[x]];
+
+            if (local_building.name)
+              if (local_building.name.toLowerCase() == building_name)
+                building_exists = [true, (!options.return_key) ? local_building : local_buildings[x]];
+          }
+        }
+      }
+    }
+
+    //If building still cannot be found by names alone, search by alias
+    if (!building_exists[0]) {
+      //Soft match first
+      for (var i = 0; i < all_building_categories.length; i++) {
+        var local_building_category = config.buildings[all_building_categories[i]];
+        var local_buildings = Object.keys(local_building_category);
+
+        for (var x = 0; x < local_buildings.length; x++) {
+          if (!["icon", "name", "order"].includes(local_buildings[x])) {
+            var local_building = local_building_category[local_buildings[x]];
+
+            if (local_building.aliases)
+              for (var y = 0; y < local_building.aliases.length; y++)
+                if (local_building.aliases[y].toLowerCase().indexOf(building_name) != -1)
+                  building_exists = [true, (!options.return_key) ? local_building : local_buildings[x]];
+          }
+        }
+      }
+
+      //Hard match second
+      for (var i = 0; i < all_building_categories.length; i++) {
+        var local_building_category = config.buildings[all_building_categories[i]];
+        var local_buildings = Object.keys(local_building_category);
+
+        for (var x = 0; x < local_buildings.length; x++) {
+          if (!["icon", "name", "order"].includes(local_buildings[x])) {
+            var local_building = local_building_category[local_buildings[x]];
+
+            if (local_building.aliases)
+              for (var y = 0; y < local_building.aliases.length; y++)
+                if (local_building.aliases[y].toLowerCase() == building_name)
+                  building_exists = [true, (!options.return_key) ? local_building : local_buildings[x]];
+          }
+        }
+      }
+    }
+
+    //Return statement
+    return (building_exists[0]) ? building_exists[1] : undefined;
+  },
+
+  //Returns all building categories based on order (if it exists)
+  getBuildingCategories: function () {
+    //Declare local instance variables
+    var all_building_categories = Object.keys(config.buildings);
+    var building_categories = [];
+    var no_order_cache = [];
+
+    //Iterate over all building categories
+    for (var i = 0; i < all_building_categories.length; i++) {
+      var local_building_category = config.buildings[all_building_categories[i]];
+
+      //Push to either no_order_cache or building_categories
+      if (local_building_category.order) {
+        if (!building_categories[local_building_category.order-1]) {
+          building_categories[local_building_category.order-1] = all_building_categories[i];
+        } else {
+          log.error(`${local_building_category[i]} has an invalid order number! .order: ${local_building_category.order-1} is already occupied by ${building_categories[local_building_category.order-1]}!\n\nCheck common/buildings for more information on resolving this error.`);
+        }
+      } else {
+        no_order_cache.push(all_building_categories[i]);
+      }
+    }
+
+    //Push no_order_cache onto the end
+    for (var i = 0; i < no_order_cache.length; i++)
+      building_categories.push(no_order_cache[i]);
+
+    //Return statement
+    return building_categories;
+  },
+
+  /*
     getBuildingCategory() - Returns back a building_category object/key based on options
     options: {
       return_key: true/false - Whether or not to return back the building category key instead of object. False by default
@@ -11,7 +143,6 @@ module.exports = {
 
     //Initialise options
     var options = (arg1_options) ? arg1_options : {};
-    if (!options.return_key) options.return_key = false;
 
     //Declare local instance variables
     var all_building_categories = Object.keys(config.buildings);
---
+++ core/framework/ui/buildings_interface.js
@@ -0,0 +1,24 @@
+module.exports = {
+  printBuildList: function (arg0_user) {
+    //Convert from parameters
+    var user_id = arg0_user;
+
+    //Declare local instance variables
+    var actual_id = main.global.user_map[user_id];
+    var game_obj = getGameObject(user_id);
+    var usr = main.users[actual_id];
+
+    //Initialise building_string
+    var building_string = [];
+
+    //Iterate over all valid buildings
+    var all_building_categories = getBuildingCategories();
+
+    for (var i = 0; i < all_building_categories.length; i++) {
+      var local_building_category = getBuildingCategory(all_building_categories[i]);
+      var local_buildings = Object.keys(local_building_category);
+
+      //Iterate over all buildings in category and push them to the list based on availability
+    }
+  }
+};
