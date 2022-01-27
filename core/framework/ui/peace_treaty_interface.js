842576803ecfea853a3660cc195f402ffaabb727
Australis <htmlprogrammingbn@gmail.com>
2022-01-27 16:54:46 +0500

    I should work on a hasOwnerChange() func for peace treaties & provs. it should return a value of the new owner, or something

--- core/framework/ui/peace_treaty_interface.js
+++ core/framework/ui/peace_treaty_interface.js
@@ -9,21 +9,47 @@ module.exports = {
 
     Also creates a new peace treaty SVG file - should be deleted when peace treaty is parsed
   */
-  modifyPeaceTreaty: function (arg0_user, arg1_peace_treaty_object) {
+  modifyPeaceTreaty: function (arg0_user, arg1_war_name, arg2_peace_treaty_object) {
     //Convert from parameters
     var user_id = arg0_user;
-    var peace_treaty_obj = arg1_peace_treaty_object;
+    var war_name = arg1_war_name.trim().toLowerCase();
+    var peace_treaty_obj = arg2_peace_treaty_object;
 
     //Declare local instance variables
     var actual_id = main.global.user_map[user_id];
+    var belligerents = [];
     var game_obj = getGameObject(user_id);
     var map_file = `${actual_id}_peace_treaty`;
     var usr = main.users[actual_id];
+    var war_obj = getWar(war_name);
 
     //Cache a new SVG
     loadMap(`${map_file}.svg`, map_file);
+
+    //Shade in the base provinces - but only for the belligerents currently involved in the war
+    for (var i = 0; i < war_obj.attackers.length; i++)
+      belligerents.push(war_obj.attackers[i]);
+    for (var i = 0; i < war_obj.defenders.length; i++)
+      belligerents.push(war_obj.defenders[i]);
+
+    for (var i = 0; i < belligerents.length; i++) {
+      var local_provinces = getProvinces(belligerents[i], { include_hostile_occupations: true });
+      var local_user = main.users[belligerents[i]];
+
+      for (var x = 0; x < local_provinces.length; x++) {
+        //Check if province will be annexed (either by retaking cores, or by outright annexation)
+
+        //Check if province owner is proposed for vassalisation
+
+        //Shade in province
+        setProvinceColour(map_file, local_provinces[x], local_user.colour);
+      }
+    }
+
+    //Initialise map viewer
     initialiseMapViewer(game_obj.id, map_file);
 
     //Visual interface using visualPrompt()
+
   }
 };
