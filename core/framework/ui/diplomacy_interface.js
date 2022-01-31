9c3d6a61fedfd599b21ed0874c696b81195e11d2
Australis <htmlprogrammingbn@gmail.com>
2022-02-01 04:50:29 +0500

    fixed initialiseViewDiplomacy()!

--- core/framework/ui/diplomacy_interface.js
+++ core/framework/ui/diplomacy_interface.js
@@ -4,6 +4,11 @@ module.exports = {
     var user_id = arg0_user;
     var do_not_change_page = arg1_do_not_change_page;
 
+    //Declare local instance variables
+    var actual_id = main.global.user_map[user_id];
+    var game_obj = getGameObject(user_id);
+    var usr = main.users[actual_id];
+
     //Declare visual prompt
     visualPrompt(game_obj.alert_embed, user_id, {
       title: `View Diplomatic Relations:`,
