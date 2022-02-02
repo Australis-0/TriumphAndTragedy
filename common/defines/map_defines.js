dd441995307bf9d78d42109404980add0e143e7d
Australis <htmlprogrammingbn@gmail.com>
2022-02-02 21:01:48 +0500

    edited test map!

--- common/defines/map_defines.js
+++ common/defines/map_defines.js
@@ -1,10 +1,10 @@
 config.defines.map = {
-  province_amount: 14102,
+  province_amount: 1711,
   map_background: "background.png",
   map_definition: "provinces.svg",
   map_overlay: "province_ids.png",
-  map_resolution: [11232, 4320],
-  map_label_coords: [20, 710],
-	map_label_placement: [20, 20, 20, 20, 10000],
-  map_type: "earth"
+  map_resolution: [4000, 2000],
+  map_label_coords: [2420, 20],
+	map_label_placement: [10, 10, 15, 20, 20, 10000],
+  map_type: "hesteria"
 };
