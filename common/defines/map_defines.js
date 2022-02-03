259416315292a386af1099356f913bd485d21685
Australis <htmlprogrammingbn@gmail.com>
2022-02-03 23:25:22 +0500

    fixed map labels!

--- common/defines/map_defines.js
+++ common/defines/map_defines.js
@@ -5,6 +5,6 @@ config.defines.map = {
   map_overlay: "province_ids.png",
   map_resolution: [4000, 2000],
   map_label_coords: [2420, 40],
-	map_label_placement: [10, 10, 15, 20, 20, 10000],
+	map_label_placement: [8, 8, 9, 10, 10000],
   map_type: "hesteria"
 };
