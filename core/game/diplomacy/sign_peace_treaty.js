e9c3c57a4aa471a809d1289b3cd649c29105d39c
Australis <htmlprogrammingbn@gmail.com>
2022-02-16 23:16:36 +0500

    fixed sign peace treaty!

--- core/game/diplomacy/sign_peace_treaty.js
+++ core/game/diplomacy/sign_peace_treaty.js
@@ -2,13 +2,14 @@ module.exports = {
   signPeaceTreaty: function (arg0_user, arg1_war_name) {
     //Convert from parameters
     var user_id = arg0_user;
-    var war_obj = (typeof arg1_war_name != "object") ?
-      getWar(war_name.trim().toLowerCase()) : arg1_war_name;
+    var war_name = arg1_war_name;
 
     //Declare local instance variables
     var actual_id = main.global.user_map[user_id];
     var game_obj = getGameObject(user_id);
     var usr = main.users[actual_id];
+    var war_obj = (typeof arg1_war_name != "object") ?
+      getWar(war_name.trim().toLowerCase()) : arg1_war_name;
 
     //Check if a war is currently active
     if (war_obj) {
