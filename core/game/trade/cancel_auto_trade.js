afb30c899d7d4e034a6f98349586ac70330723a8
Australis <htmlprogrammingbn@gmail.com>
2022-07-28 12:56:37 +0500

    Update cancel_auto_trade.js

--- core/game/trade/cancel_auto_trade.js
+++ core/game/trade/cancel_auto_trade.js
@@ -1,11 +1,12 @@
 module.exports = {
-  cancelAutoTrade: function (arg0_user, arg1_key) { //[WIP] - Remember to update auto-trade UI when being focused on by user
+  cancelAutoTrade: function (arg0_user, arg1_key) {
     //Convert from parameters
     var user_id = arg0_user;
     var auto_trade_key = arg1_key;
 
     //Declare local instance variables
     var actual_id = main.global.user_map[user_id];
+    var game_obj = getGameObject(user_id);
     var usr = main.users[actual_id];
 
     var selected_auto_trade = usr.auto_trades[auto_trade_key];
