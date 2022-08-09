88496b5d2b4f4d38a04b548fe693d12b46d64fd5
Australis <htmlprogrammingbn@gmail.com>
2022-08-09 22:25:52 +0500

    fixed mod loc formatting!

--- core/framework/data/modifier_framework.js
+++ core/framework/data/modifier_framework.js
@@ -455,7 +455,7 @@ module.exports = {
 
               break;
             case "reform_desire_gain":
-              modifier_string.push(`${prefix}Reform Desire Gain: **${printPercentage(local_value[0], { base_zero: true })}**`);
+              modifier_string.push(`${prefix}**${printPercentage(local_value[0], { base_zero: true })}** Reform Desire Gain`);
 
               break;
             case "set_mobilisation_unit":
@@ -464,7 +464,7 @@ module.exports = {
 
               break;
             case "stability_modifier":
-              modifier_string.push(`${prefix}Stability Modifier: **${printPercentage(local_value[0], { base_zero: true })}**`);
+              modifier_string.push(`${prefix}**${printPercentage(local_value[0], { base_zero: true })}** Stability Modifier`);
 
               break;
             case "unlock_building":
