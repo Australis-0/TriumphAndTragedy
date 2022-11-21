4b8a1353f289bac8e3fc3a74677111673dccfafb
Australis <htmlprogrammingbn@gmail.com>
2022-11-21 06:38:14 +0500

    fixed loc!

--- localisation/combat_modifiers.js
+++ localisation/combat_modifiers.js
@@ -1,6 +1,6 @@
 config.localisation.attack = "Attack";
 config.localisation.defence = "Defence";
-config.localisation.initiative = "initiative";
+config.localisation.initiative = "Initiative";
 config.localisation.manoeuvre = "Manoeuvre";
 config.localisation.movement = "km/h Speed";
 config.localisation.range = "Range";
