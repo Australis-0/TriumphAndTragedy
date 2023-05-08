32c5264ea6432ec9cdb22511d0ba9dd5c3322f86
Australis <htmlprogrammingbn@gmail.com>
2023-05-08 18:19:42 +0500

    revamped admin/agriculture buildings config!

---
+++ common/buildings/administration.js
@@ -0,0 +1,494 @@
+config.buildings.administration = {
+  order: 1,
+  city_halls: {
+    name: "City Halls",
+    singular: "City Hall",
+
+    construction_turns: 4,
+    cost: {
+      lumber: 20,
+      bricks: 15,
+      common_furniture: 10,
+      steel_beams: 10,
+      lamps: 5,
+      luxury_furniture: 5,
+      marble: 5,
+      glass: 5,
+      money: 20000
+    },
+    maintenance: {
+      money: 500,
+      paper: 3,
+      ink: 2
+    },
+    manpower_cost: {
+      bureaucrats: 1000
+    },
+    modifiers: {
+      local_production_efficiency: 0.15,
+      local_housing: 0.10,
+      local_rgo_throughput: 0.05,
+      bureaucrat_pop_chance: 0.01,
+      districts: 2
+    }
+  },
+  courthouses: {
+    name: "Courthouses",
+    singular: "Courthouse",
+
+    construction_turns: 3,
+    cost: {
+      lumber: 15,
+      luxury_furniture: 10,
+      glass: 10,
+      mahogany_lumber: 5,
+      lamps: 5,
+      marble: 5,
+      velvet: 3,
+      money: 5000
+    },
+    maintenance: {
+      money: 2500,
+      lumber: 5,
+      paper: 5,
+      lamps: 4,
+      ink: 3,
+      foods: 2,
+      alcohol: 2,
+      salt: 2,
+      naval_supplies: 1
+    },
+    manpower_cost: {
+      bureaucrats: 500
+    },
+    modifiers: {
+      cultural_integration_speed: 0.20,
+      local_manpower: 0.05,
+      bureaucrat_pop_chance: 0.01,
+      local_corruption: -0.10,
+      local_crime_rate: -0.50,
+      local_title_strength: -5
+    }
+  },
+  harbourmasters: {
+    name: "Harbourmasters",
+    singular: "Harbourmaster",
+    aliases: ["harbormasters"],
+
+    construction_turns: 4,
+    cost: {
+      lumber: 20,
+      stone: 15,
+      bricks: 10,
+      common_furniture: 10,
+      glass: 5,
+      lamps: 5,
+      wood: 5,
+      steel_beams: 5,
+      copper: 3,
+      iron: 2,
+      money: 10000
+    },
+    manpower_cost: {
+      bureaucrats: 50,
+      artisans: 100,
+      soldiers: 50
+    },
+    maintenance: {
+      money: 2500,
+      lumber: 5,
+      paper: 5,
+      lamps: 4,
+      ink: 3,
+      foods: 3,
+      alcohol: 2,
+      salt: 2,
+      naval_supplies: 1
+    },
+    modifiers: {
+      local_military_bonus: 0.20,
+      local_stockpiles_bonus: 0.20,
+      local_shipment_capacity_bonus: 0.10,
+      local_exported_goods_price: 0.05,
+      local_imported_goods_price: 0.05,
+      local_manpower: 0.05,
+      naval_attack: 0.01,
+      naval_defence: 0.01,
+      local_industry_slots: 3
+    }
+  },
+  local_departments: {
+    name: "Local Departments",
+    singular: "Local Department",
+
+    construction_turns: 2,
+    cost: {
+      bricks: 20,
+      glass: 20,
+      concrete: 15,
+      lumber: 15,
+      ceramics: 10,
+      iron: 10,
+      stone: 5,
+      lamps: 4,
+      bronze: 2,
+      brass: 1,
+      money: 3500
+    },
+    manpower_cost: {
+      bureaucrats: 1000
+    },
+    maintenance: {
+      money: 1500,
+      paper: 5,
+      ink: 3,
+      lamps: 2,
+      leather: 2,
+      tools: 2
+    },
+    modifiers: {
+      local_exported_goods_price: 0.10,
+      local_stockpiles_bonus: 0.10,
+      local_corruption: -0.05,
+      workforce_requirement: -0.05
+    }
+  },
+  national_departments: {
+    name: "National Departments",
+    singular: "National Department",
+
+    construction_turns: 4,
+    cost: {
+      bricks: 25,
+      glass: 20,
+      concrete: 15,
+      lumber: 15,
+      ceramics: 10,
+      marble: 10,
+      iron: 10,
+      lead: 7,
+      stone: 5,
+      lamps: 4,
+      bronze: 2,
+      slate: 2,
+      chinaware: 2,
+      brass: 1,
+      money: 20000
+    },
+    manpower_cost: {
+      bureaucrats: 2000
+    },
+    maintenance: {
+      money: 1500,
+      paper: 5,
+      ink: 3,
+      lamps: 2,
+      leather: 2,
+      tools: 2
+    },
+    modifiers: {
+      national_stockpile: 0.50,
+      money_per_action: 10,
+      global_supply_limit: 5,
+      political_capital_gain: 2
+    }
+  },
+  mental_institutions: {
+    name: "Mental Institutions",
+    singular: "Mental Institution",
+
+    construction_turns: 3,
+    cost: {
+      glass: 20,
+      stone: 15,
+      bricks: 10,
+      iron: 8,
+      steel_beams: 5,
+      concrete: 5,
+      common_furniture: 4,
+      telephones: 2,
+      computers: 1,
+      money: 7000
+    },
+    maintenance: {
+      money: 2500,
+      paper: 3,
+      ink: 3,
+      staple_clothes: 3,
+      felt: 2,
+      cosmetic_products: 1,
+      fabric: 1,
+      telephones: 1,
+      lightbulbs: 1,
+      hormone_medications: 1,
+      mood_stabilisers: 1,
+      plastics: 1,
+      stimulants: 1,
+      shampoo: 1,
+      soap: 1
+    },
+    manpower_cost: {
+      bureaucrats: 500,
+      faculty: 150,
+      scientists: 50
+    },
+    modifiers: {
+      mental_health: 0.05,
+      local_production_efficiency: -0.02,
+      impact_from_mental_health_conditions: -0.10,
+      political_capital_gain: -1
+    }
+  },
+  palaces: {
+    name: "Palaces",
+    singular: "Palace",
+
+    construction_turns: 8,
+    cost: {
+      marble: 25,
+      glass: 20,
+      bricks: 20,
+      iron: 15,
+      lead: 15,
+      wood_veneers: 10,
+      stone: 10,
+      lamps: 9,
+      silver: 8,
+      lumber: 8,
+      bronze: 7,
+      luxury_furniture: 7,
+      velvet: 5,
+      gold: 5,
+      brass: 5,
+      slate: 4,
+      chinaware: 3,
+      money: 50000
+    },
+    maintenance: {
+      money: 20000,
+      luxury_clothes: 5,
+      paper: 4,
+      ink: 3,
+      tea: 3,
+      foods: 3,
+      coffee: 2,
+      cosmetic_products: 2,
+      lumber: 2,
+      red_wine: 2,
+      soap: 2,
+      cheese: 1,
+      glasses: 1,
+      incense: 1,
+      porcelain: 1,
+      tools: 1,
+      white_wine: 1
+    },
+    modifiers: {
+      local_tax_efficiency: 0.10,
+      administrative_efficiency: 0.05,
+      local_manpower: 0.05,
+      tax_efficiency: 0.02,
+      aristocrat_needs: -0.10,
+      title_strength: -2
+    }
+  },
+  prisons: {
+    name: "Prisons",
+    singular: "Prison",
+
+    construction_turns: 4,
+    cost: {
+      bricks: 20,
+      stone: 15,
+      iron: 10,
+      lead: 8,
+      common_furniture: 3,
+      lamps: 2,
+      brass: 2,
+      small_arms: 1,
+      ammunition: 1,
+      money: 8500
+    },
+    maintenance: {
+      money: 2000,
+      bread: 3,
+      uniforms: 2,
+      soap: 2,
+      lumber: 1,
+      tools: 1
+    },
+    manpower_cost: {
+      soldiers: 250,
+      bureaucrats: 200
+    },
+    modifiers: {
+      law_enforcement_effectiveness: 0.05,
+      local_crime_rate: 0.05,
+      local_consciousness: -0.15
+    }
+  },
+  state_houses: {
+    name: "State Houses",
+    singular: "State House",
+
+    construction_turns: 6,
+    cost: {
+      lumber: 15,
+      marble: 10,
+      bricks: 8,
+      stone: 5,
+      wood_veneers: 5,
+      luxury_furniture: 4,
+      lamps: 2,
+      velvet: 2,
+      chinaware: 1,
+      money: 5000
+    },
+    maintenance: {
+      money: 5000,
+      paper: 5,
+      ink: 3,
+      lamps: 3
+    },
+    manpower_cost: {
+      bureaucrats: 1000,
+      aristocrats: 500
+    },
+    modifiers: {
+      local_tax_efficiency: 0.10,
+      syphoned_revenue: -0.20,
+      syphoned_production: -0.20,
+      local_infrastructure_slots: 1,
+      state_limit: 1,
+      local_title_strength: -20
+    },
+    produces: {
+      bureaucrats: 500
+    }
+  },
+  town_halls: {
+    name: "Town Halls",
+    singular: "Town Hall",
+
+    construction_turns: 3,
+    cost: {
+      lumber: 20,
+      bricks: 15,
+      stone: 10,
+      lead: 5,
+      glass: 5,
+      iron: 5,
+      wood_veneers: 5,
+      common_furniture: 3,
+      lamps: 3,
+      money: 2000
+    },
+    maintenance: {
+      money: 2000,
+      paper: 3,
+      ink: 2,
+      lamps: 2
+    },
+    manpower_cost: {
+      artisans: 250,
+      bureaucrats: 100
+    },
+    modifiers: {
+      local_happiness: 0.10,
+      local_wages: 0.05,
+      local_corruption: -0.05,
+      amenity_districts: 3
+    }
+  },
+  trade_depots: {
+    name: "Trade Depots",
+    singular: "Trade Depot",
+
+    construction_turns: 4,
+    cost: {
+      stone: 15,
+      bricks: 10,
+      lumber: 8,
+      iron: 5,
+      money: 1000
+    },
+    maintenance: {
+      money: 1500,
+      tools: 3,
+      lumber: 2,
+      paper: 2,
+      ink: 1,
+      naval_supplies: 1
+    },
+    manpower_cost: {
+      artisans: 100,
+      soldiers: 50
+    },
+    modifiers: {
+      national_stockpile_bonus: 0.05,
+      shipment_capacity: 50,
+      trading_slots: 1
+    }
+  },
+  trade_unions: {
+    name: "Trade Unions",
+    singular: "Trade Union",
+
+    construction_turns: 3,
+    cost: {
+      lumber: 20,
+      bricks: 5,
+      common_furniture: 5,
+      money: 1000
+    },
+    maintenance: {
+      money: 500,
+      paper: 2,
+      ink: 2,
+      lamps: 1,
+      tools: 1,
+      typewriters: 1,
+      coals: 1
+    },
+    manpower_cost: {
+      labourers: 1000,
+      artisans: 350
+    },
+    modifiers: {
+      local_consciousness: 0.10,
+      local_wages: 0.05,
+      local_corruption: 0.01,
+      social_mobility: 0.01,
+      workforce_requirement: -0.05,
+      local_amenities_slots: 1,
+      political_capital_gain: -1
+    }
+  },
+  warehouses: {
+    name: "Warehouses",
+    singular: "Warehouse",
+
+    construction_turns: 2,
+    cost: {
+      bricks: 10,
+      lumber: 10,
+      stone: 8,
+      iron: 5,
+      lead: 3,
+      money: 5000
+    },
+    maintenance: {
+      money: 1000,
+      tools: 3,
+      lumber: 2
+    },
+    manpower_cost: {
+      artisans: 200
+    },
+    modifiers: {
+      local_import_needs: -0.05,
+      local_stockpile: 100,
+      supply_limit: 10
+    }
+  }
+};
--- common/buildings/agriculture.js
+++ common/buildings/agriculture.js
@@ -1,169 +1,620 @@
 config.buildings.agriculture = {
-  order: 1,
+  order: 2,
+  beekeepers: {
+    name: "Beekeepers",
+    singular: "Beekeeper",
+
+    construction_turns: 2,
+    cost: {
+      lumber: 5,
+      wood: 3,
+      money: 500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 100,
+        farmers: 100
+      }
+    },
+    produces: {
+      beeswax: 3
+    }
+  },
+  camphor_plantations: {
+    name: "Camphor Plantations",
+    singular: "Camphor Plantation",
+
+    construction_turns: 2,
+    cost: {
+      lumber: 4,
+      wood: 2,
+      money: 800
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 250,
+        farmers: 250
+      }
+    },
+    produces: {
+      camphor: 3
+    }
+  },
+  carmine_harvesters: {
+    name: "Carmine Harvesters",
+    singular: "Carmine Harvester",
+
+    construction_turns: 3,
+    cost: {
+      wood: 3,
+      money: 1200
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 1000,
+        farmers: 1000
+      }
+    },
+    produces: {
+      carmine_bugs: 2
+    }
+  },
+  cinnamon_plantations: {
+    name: "Cinnamon Plantations",
+    singular: "Cinnamon Plantation",
+
+    construction_turns: 3,
+    cost: {
+      lumber: 6,
+      stone: 5,
+      lamps: 2,
+      wood: 2,
+      common_furniture: 2,
+      money: 2500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 500,
+        farmers: 500
+      }
+    },
+    produces: {
+      cinnamon: 2
+    }
+  },
+  clove_plantations: {
+    name: "Clove Plantations",
+    singular: "Clove Plantation",
+
+    construction_turns: 3,
+    cost: {
+      stone: 5,
+      lumber: 4,
+      lamps: 2,
+      wood: 2,
+      common_furniture: 2,
+      money: 2500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 500,
+        farmers: 500
+      }
+    },
+    produces: {
+      cloves: 2
+    }
+  },
   farms: {
     name: "Farms",
     singular: "Farm",
-    icon: "farms",
 
-    construction_turns: 1,
+    construction_turns: 3,
     cost: {
       wood: 15,
       stone: 5,
       money: 2000
     },
     manpower_cost: {
-      labourers: 50000
+      any_pop: {
+        peasants: 10000,
+        serfs: 10000,
+        farmers: 10000
+      }
     },
     produces: {
-      wheat: 2,
-      meat: 1
+      production_choice_corn: {
+        corn: 7
+      },
+      production_choice_potatoes: {
+        potatoes: 8
+      },
+      production_choice_teff: {
+        teff: 2
+      },
+      production_choice_wheat: {
+        wheat: 5
+      },
+      production_choice_blue_agaves: {
+        blue_agaves: 4
+      },
+      production_choice_hops: {
+        hops: 3
+      },
+      production_bell_peppers: {
+        bell_peppers: 4
+      },
+      production_choice_garlic: {
+        garlic: 5
+      },
+      production_choice_ginger: {
+        ginger: 4
+      },
+      production_choice_linseed: {
+        linseed: 4
+      },
+      production_choice_rapeseed: {
+        rapeseed: 3
+      }
     }
   },
-  centralised_farms: {
-    name: "Centralised Farms",
-    singular: "Centralised Farm",
-    icon: "centralised_farms",
+  fruit_farms: {
+    name: "Fruit Farms",
+    singular: "Fruit Farm",
+
+    construction_turns: 2,
+    cost: {
+      wood: 10,
+      stone: 5,
+      money: 1500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 10000,
+        serfs: 10000,
+        farmers: 10000
+      }
+    },
+    produces: {
+      production_choice_apples: {
+        apples: 6
+      },
+      production_choice_grapes: {
+        grapes: 5
+      },
+      production_choice_olives: {
+        olives: 4
+      },
+      production_choice_pumpkins: {
+        pumpkins: 2
+      },
+      production_choice_strawberries: {
+        strawberries: 5
+      },
+      production_choice_sunflowers: {
+        sunflowers: 3
+      },
+      production_choice_watermelons: {
+        watermelons: 5
+      }
+    }
+  },
+  illicit_fields: {
+    name: "Illicit Fields",
+    singular: "Illicit Field",
+
+    construction_turns: 1,
+    cost: {
+      wood: 5,
+      stone: 5,
+      lamps: 3,
+      common_furniture: 2,
+      tools: 2,
+      glass: 1,
+      iron: 1,
+      money: 500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 25000,
+        serfs: 25000,
+        farmers: 25000
+      }
+    },
+    produces: {
+      production_choice_hemp: {
+        hemp: 2
+      },
+      production_choice_poppies: {
+        poppies: 3
+      }
+    }
+  },
+  indigo_fields: {
+    name: "Indigo Fields",
+    singular: "Indigo Field",
+
+    construction_turns: 4,
+    cost: {
+      stone: 5,
+      lumber: 4,
+      lamps: 2,
+      wood: 2,
+      common_furniture: 2,
+      money: 2500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 2000,
+        farmers: 2000
+      }
+    },
+    produces: {
+      indigo: 3
+    }
+  },
+  livestock_farms: {
+    name: "Livestock Farms",
+    singular: "Livestock Farm",
 
     construction_turns: 2,
     cost: {
       lumber: 10,
       iron: 5,
-      sulphur: 5,
-      money: 4000
+      tools: 3,
+      wood: 2,
+      lamps: 1,
+      money: 2000
     },
     manpower_cost: {
-      labourers: 25000
+      any_pop: {
+        peasants: 10000,
+        serfs: 10000,
+        farmers: 10000
+      }
     },
     produces: {
-      wheat: 3,
-      meat: 2
+      production_choice_eggs: {
+        eggs: 5
+      },
+      production_choice_leather: {
+        leather: 1
+      },
+      production_choice_milk: {
+        milk: 5
+      },
+      production_choice_feathers: {
+        feathers: 1
+      },
+      production_choice_beef: {
+        beef: 4
+      },
+      production_choice_chicken: {
+        chicken: 4
+      },
+      production_choice_cuy: {
+        cuy: 1
+      },
+      production_choice_duck: {
+        duck: 3
+      },
+      production_choice_goat_meat: {
+        goat_meat: 2
+      },
+      production_choice_mutton: {
+        mutton: 3
+      },
+      production_choice_pork: {
+        pork: 5
+      },
+      production_choice_rabbit_meat: {
+        rabbit_meat: 2
+      }
     }
   },
-  modern_farms: {
-    name: "Modern Farms",
-    singular: "Modern Farm",
-    icon: "modern_farms",
-
-    construction_turns: 2,
-    cost: {
-      steel: 10,
-      machine_parts: 5,
-      lumber: 5,
-      stone: 5,
-      money: 10000
-    },
-    maintenance: {
-      fertiliser: 2,
-      refined_petroil: 1
-    },
-    manpower_cost: {
-      labourers: 25000
-    },
-    produces: {
-      wheat: 5,
-      meat: 3
-    }
-  },
-  industrial_farms: {
-    name: "Industrial Farms",
-    singular: "Industrial Farm",
-    icon: "industrial_farms",
+  mustard_plantations: {
+    name: "Mustard Plantations",
+    singular: "Mustard Plantation",
 
     construction_turns: 3,
     cost: {
-      steel: 15,
-      concrete: 10,
-      machine_parts: 10,
-      lumber: 5,
-      money: 15000
-    },
-    maintenance: {
-      fertiliser: 3,
-      refined_petroil: 2
+      wood: 5,
+      lumber: 3,
+      stone: 2,
+      common_furniture: 2,
+      glass: 1,
+      lamps: 1,
+      money: 1500
     },
     manpower_cost: {
-      labourers: 15000
+      any_pop: {
+        peasants: 3000,
+        serfs: 3000,
+        farmers: 3000
+      }
     },
     produces: {
-      wheat: 5,
-      meat: 5
+      mustard: 3
     }
   },
-  vertical_farms: {
-    name: "Vertical Farms",
-    singular: "Vertical Farm",
-    icon: "vertical_farms",
+  nut_farms: {
+    name: "Nut Farms",
+    singular: "Nut Farm",
 
-    construction_turns: 5,
+    construction_turns: 2,
     cost: {
-      steel: 20,
-      concrete: 10,
-      machine_parts: 15,
-      gold: 3,
-      money: 20000
-    },
-    maintenance: {
-      machine_parts: 2
+      lumber: 8,
+      wood: 5,
+      stone: 3,
+      common_furniture: 2,
+      lamps: 2,
+      tools: 1,
+      money: 800
     },
     manpower_cost: {
-      labourers: 10000
+      any_pop: {
+        peasants: 5000,
+        serfs: 5000,
+        farmers: 5000
+      }
     },
     produces: {
-      wheat: 10,
-      meat: 5
+      production_choice_peanuts: {
+        peanuts: 4
+      },
+      production_choice_walnuts: {
+        walnuts: 2
+      }
+    }
+  },
+  palm_oil_plantations: {
+    name: "Palm Oil Plantations",
+    singular: "Palm Oil Plantation",
+
+    construction_turns: 4,
+    cost: {
+      wood: 20,
+      lumber: 15,
+      tools: 10,
+      lamps: 5,
+      money: 5000
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 5000,
+        serfs: 5000,
+        farmers: 5000
+      }
+    },
+    produces: {
+      oil_palm_fruit: 3
     }
   },
   pastures: {
     name: "Pastures",
     singular: "Pasture",
-    icon: "pastures",
 
-    construction_turns: 1,
+    construction_turns: 2,
     cost: {
       wood: 10,
       money: 1200
     },
     manpower_cost: {
-      labourers: 15000
+      any_pop: {
+        peasants: 15000,
+        serfs: 15000,
+        farmers: 15000
+      }
     },
     produces: {
-      meat: 2
+      production_choice_goat_milk: {
+        goat_milk: 3
+      },
+      production_choice_milk: {
+        milk: 5
+      },
+      production_choice_camel_meat: {
+        camel_meat: 1
+      },
+      production_choice_reindeer: {
+        reindeer: 1
+      },
+      production_choice_venison: {
+        venison: 2
+      },
+      production_choice_yak: {
+        yak: 3
+      },
+      production_choice_alpaca_wool: {
+        alpaca_wool: 3
+      },
+      production_choice_goat_wool: {
+        goat_wool: 2
+      },
+      production_choice_sheep_wool: {
+        sheep_wool: 5
+      }
     }
   },
-  fisheries: {
-    name: "Fisheries",
-    singular: "Fishery",
-    icon: "fisheries",
+  spice_plantations: {
+    name: "Spice Plantations",
+    singular: "Spice Plantation",
 
-    construction_turns: 1,
+    construction_turns: 3,
     cost: {
-      wood: 5,
-      iron: 5,
-      money: 400
+      stone: 5,
+      lumber: 4,
+      lamps: 2,
+      wood: 2,
+      common_furniture: 2,
+      money: 2500
     },
     manpower_cost: {
-      labourers: 35000
+      any_pop: {
+        peasants: 1500,
+        farmers: 1500
+      }
     },
     produces: {
-      meat: 3
+      production_choice_cardamom: {
+        cardamom: 2
+      },
+      production_choice_cayenne: {
+        cayenne: 2
+      },
+      production_choice_chilli: {
+        chilli: 2
+      },
+      production_choice_fennel: {
+        fennel: 2
+      },
+      production_choice_fenugreek: {
+        fenugreek: 3
+      },
+      production_choice_nutmeg: {
+        nutmeg: 2
+      },
+      production_choice_paprika: {
+        paprika: 4
+      },
+      production_choice_pepper: {
+        pepper: 5
+      }
     }
   },
-  plantations: {
-    name: "Plantations",
-    singular: "Plantation",
-    icon: "plantations",
+  tropical_plantations: {
+    name: "Tropical Plantations",
+    singular: "Tropical Plantation",
+
+    construction_turns: 3,
+    cost: {
+      stone: 5,
+      lumber: 4,
+      iron: 3,
+      tools: 3,
+      lamps: 2,
+      wood: 2,
+      common_furniture: 2,
+      money: 2500
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 1500,
+        farmers: 1500
+      }
+    },
+    produces: {
+      production_choice_cacao: {
+        cacao: 4
+      },
+      production_choice_coffee: {
+        coffee: 3
+      },
+      production_choice_quina: {
+        quina: 3
+      },
+      production_choice_tobacco: {
+        tobacco: 5
+      },
+      production_choice_bananas: {
+        bananas: 4
+      },
+      production_choice_citrus: {
+        citrus: 3
+      },
+      production_choice_dates: {
+        dates: 2
+      },
+      production_choice_guavas: {
+        guavas: 2
+      },
+      production_choice_mangoes: {
+        mangoes: 3
+      },
+      production_choice_mangosteens: {
+        mangosteens: 2
+      },
+      production_choice_sweet_potatoes: {
+        sweet_potatoes: 4
+      },
+      production_choice_yam: {
+        yam: 5
+      }
+    }
+  },
+  vegetable_fields: {
+    name: "Vegetable Fields",
+    singular: "Vegetable Field",
 
     construction_turns: 2,
     cost: {
-      wood: 15,
+      wood: 10,
       stone: 5,
-      money: 5000
+      money: 1500
     },
     manpower_cost: {
-      labourers: 25000
+      any_pop: {
+        peasants: 10000,
+        farmers: 10000,
+        serfs: 10000
+      }
     },
     produces: {
-      cotton: 15
+      production_choice_cabbage: {
+        cabbage: 5
+      },
+      production_choice_carrot: {
+        carrot: 4
+      },
+      production_choice_chicory: {
+        chicory: 2
+      },
+      production_choice_cucumbers: {
+        cucumbers: 3
+      },
+      production_choice_eggplants: {
+        eggplants: 3
+      },
+      production_choice_lettuce: {
+        lettuce: 5
+      },
+      production_choice_onions: {
+        onions: 5
+      },
+      production_choice_peas: {
+        peas: 4
+      },
+      production_choice_soybeans: {
+        soybeans: 3
+      },
+      production_choice_spinach: {
+        spinach: 4
+      },
+      production_choice_tomatoes: {
+        tomatoes: 6
+      },
+      production_choice_turnips: {
+        turnips: 7
+      }
+    }
+  },
+  woad_harvesters: {
+    name: "Woad Harvesters",
+    singular: "Woad Harvester",
+
+    construction_turns: 2,
+    cost: {
+      wood: 3,
+      money: 1200
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 1000,
+        farmers: 1000
+      }
+    },
+    produces: {
+      woad: 2
     }
   }
 };
