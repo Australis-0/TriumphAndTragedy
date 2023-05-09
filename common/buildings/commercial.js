0e171575055a6ed5fdc5da46a28556e53c3aa8ef
Australis <htmlprogrammingbn@gmail.com>
2023-05-09 07:22:12 +0500

    added commercial & edu buildings to config!

---
+++ common/buildings/commercial.js
@@ -0,0 +1,449 @@
+config.buildings.commercial = { //[WIP] - Add special_effect
+  order: 4,
+
+  banks: {
+    name: "Banks",
+    singular: "Bank",
+
+    construction_turns: 5,
+    cost: {
+      iron: 50,
+      glass: 30,
+      bricks: 25,
+      ceramics: 20,
+      marble: 15,
+      limestone: 10,
+      lamps: 8,
+      common_furniture: 5,
+      money: 35000
+    },
+    maintenance: {
+      money: 5000,
+      lamps: 4,
+      paper: 3,
+      ink: 3,
+      iron: 2,
+      tools: 2,
+      computers: 1,
+      gold: 1,
+      telephones: 1
+    },
+    manpower_cost: {
+      capitalists: 2000,
+      engineers: 1500
+    }
+  },
+  central_business_districts: {
+    name: "Central Business Districts",
+    singular: "Central Business District",
+    aliases: ["CBD"],
+
+    construction_turns: 8,
+    cost: {
+      reinforced_concrete: 35,
+      ceramics: 25,
+      lumber: 20,
+      steel_beams: 20,
+      lightbulbs: 10,
+      marble: 5,
+      electric_gear: 8,
+      machine_parts: 5,
+      lifts: 5,
+      computers: 2,
+      wood_veneers: 2
+    },
+    maintenance: {
+      money: 8500,
+      machine_parts: 8,
+      common_furniture: 5,
+      glass: 5,
+      computers: 4,
+      lumber: 4,
+      paper: 3,
+      ink: 3,
+      electric_gear: 2,
+      luxury_furniture: 2,
+      typewriters: 2,
+      lifts: 1
+    },
+    manpower_cost: {
+      capitalists: 50000,
+      engineers: 20000,
+      labourers: 5000
+    },
+    produces: {
+      money_per_action: 150
+    }
+  },
+  chemists: {
+    name: "Chemists",
+    singular: "Chemist",
+    aliases: ["pharmacy"],
+
+    construction_turns: 2,
+    cost: {
+      bricks: 10,
+      lumber: 10,
+      stone: 8,
+      glass: 5,
+      common_furniture: 3,
+      money: 7500
+    },
+    maintenance: {
+      money: 500,
+      pharmaceuticals: 4,
+      traditional_medicines: 1
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  counting_houses: {
+    name: "Counting Houses",
+    singular: "Counting House",
+
+    construction_turns: 4,
+    cost: {
+      bricks: 15,
+      ceramics: 10,
+      stone: 10,
+      limestone: 5,
+      marble: 5,
+      glass: 5,
+      common_furniture: 3,
+      lamps: 2,
+      money: 10000
+    },
+    maintenance: {
+      money: 5000,
+      paper: 4,
+      ink: 4,
+      common_furniture: 2,
+      lamps: 2
+    },
+    manpower_cost: {
+      artisans: 8000,
+      bureaucrats: 5000
+    }
+  },
+  department_stores: {
+    name: "Department Stores",
+    singular: "Department Store",
+
+    construction_turns: 4,
+    cost: {
+      bricks: 35,
+      marble: 20,
+      iron: 20,
+      limestone: 15,
+      concrete: 10,
+      cement: 10,
+      common_furniture: 8,
+      stone: 5,
+      machine_parts: 5,
+      lightbulbs: 3,
+      money: 25000
+    },
+    maintenance: {
+      money: 6500,
+      furniture: 10,
+      cosmetic_products: 8,
+      foods: 8,
+      clothes: 6,
+      lamps: 6,
+      glass: 5,
+      electric_gear: 5,
+      shampoo: 5,
+      soap: 5,
+      machine_parts: 4,
+      lightbulbs: 3,
+      copper_wires: 2,
+      typewriters: 2,
+      computers: 1,
+      radios: 1,
+      telephones: 1,
+      televisions: 1
+    },
+    manpower_cost: {
+      labourers: 20000,
+      engineers: 5000
+    },
+    modifiers: {
+      variety_bonus_upper_class: 0.15,
+      variety_bonus_middle_class: 0.15
+    },
+    produces: {
+      money: 8500
+    }
+  },
+  furniture_stores: {
+    name: "Furniture Stores",
+    singular: "Furniture Store",
+
+    construction_turns: 2,
+    cost: {
+      lumber: 15,
+      bricks: 10,
+      common_furniture: 8,
+      stone: 5,
+      glass: 5,
+      money: 9000
+    },
+    maintenance: {
+      money: 700,
+      common_furniture: 8,
+      luxury_furniture: 2,
+      sewing_machines: 2,
+      lightbulbs: 1,
+      typewriters: 1,
+      computers: 1,
+      radios: 1,
+      telephones: 1,
+      televisions: 1
+    },
+    manpower_cost: {
+      labourers: 15000
+    },
+    modifiers: {
+      furniture_export_price: 0.05,
+      furniture_import_price: -0.05,
+    },
+    produces: {
+      money: 5000
+    }
+  },
+  malls: {
+    name: "Malls",
+    singular: "Mall",
+
+    construction_turns: 3,
+    cost: {
+      glass: 15,
+      bricks: 10,
+      ceramics: 10,
+      stone: 8,
+      lightbulbs: 5,
+      common_furniture: 4,
+      machine_parts: 2,
+      money: 12000
+    },
+    maintenance: {
+      money: 850,
+      clothes: 5,
+      common_furniture: 3,
+      foods: 2,
+      chinaware: 1,
+      paper: 1,
+      wood_veneers: 1
+    },
+    manpower_cost: {
+      labourers: 30000
+    },
+    produces: {
+      money: 3500
+    }
+  },
+  markets: {
+    name: "Markets",
+    singular: "Market",
+
+    construction_turns: 2,
+    cost: {
+      linen: 8,
+      lumber: 5,
+      stone: 5,
+      money: 1500
+    },
+    maintenance: {
+      money: 500,
+      foods: 3,
+      vegetables: 2,
+      fruits: 2,
+      animal_products: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        peasants: 20000,
+        farmers: 20000,
+        labourers: 20000
+      }
+    },
+    modifiers: {
+      local_lower_class_needs: -0.10
+    },
+    produces: {
+      money: 2000
+    }
+  },
+  modern_financial_centres: {
+    name: "Modern Financial Centres",
+    singular: "Modern Financial Centre",
+    aliases: ["modern financial center"],
+
+    construction_turns: 6,
+    cost: {
+      glass: 30,
+      steel_beams: 25,
+      bricks: 20,
+      ceramics: 20,
+      reinforced_concrete: 20,
+      concrete: 15,
+      cement: 10,
+      marble: 10,
+      integrated_circuits: 9,
+      electric_gear: 8,
+      machine_parts: 8,
+      computers: 7,
+      telephones: 7,
+      televisions: 6,
+      plastics: 5,
+      brass: 3,
+      bronze: 2,
+      luxury_furniture: 2,
+      wood_veneers: 2,
+      money: 50000
+    },
+    maintenance: {
+      money: 5000,
+      telephones: 6,
+      computers: 5,
+      lightbulbs: 4,
+      typewriters: 3,
+      ink: 3,
+      paper: 3,
+      electric_gear: 2,
+      plastics: 2,
+      lifts: 1
+    },
+    manpower_cost: {
+      capitalists: 20000,
+      engineers: 15000,
+      artisans: 5000,
+      labourers: 5000
+    },
+    modifiers: {
+      tax_efficiency: 0.02
+    },
+    produces: {
+      money: 25000
+    }
+  },
+  restaurants: {
+    name: "Restaurants",
+    singular: "Restaurant",
+
+    construction_turns: 3,
+    cost: {
+      lumber: 15,
+      bricks: 10,
+      stone: 8,
+      glass: 6,
+      chinaware: 5,
+      common_furniture: 3,
+      money: 3500
+    },
+    maintenance: {
+      money: 500,
+      foods: 3,
+      meat: 2,
+      vegetables: 2,
+      animal_products: 1,
+      seasoning: 1,
+      sugar: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        labourers: 10000,
+        peasants: 10000
+      }
+    },
+    modifiers: {
+      local_tourism_satisfaction: 0.10,
+      local_tax_efficiency: 0.05
+    },
+    produces: {
+      money: 2500
+    }
+  },
+  shopping_centres: {
+    name: "Shopping Centres",
+    singular: "Shopping Centre",
+    aliases: ["shopping center"],
+
+    construction_turns: 4,
+    cost: {
+      bricks: 10,
+      reinforced_concrete: 8,
+      iron: 8,
+      ceramics: 8,
+      asphalt: 6,
+      glass: 5,
+      dyes: 2,
+      money: 8500
+    },
+    maintenance: {
+      money: 1000,
+      foods: 6,
+      lamps: 4,
+      staple_clothes: 3,
+      common_furniture: 3,
+      lightbulbs: 2,
+      luxury_clothes: 2,
+      plastics: 2,
+      chinaware: 1,
+      cosmetic_products: 1,
+      paper: 1,
+      soap: 1,
+      shampoo: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        labourers: 10000,
+        peasants: 10000
+      }
+    }
+  },
+  stock_exchanges: {
+    name: "Stock Exchanges",
+    singular: "Stock Exchange",
+    aliases: ["stock market"],
+
+    construction_turns: 6,
+    cost: {
+      iron: 15,
+      stone: 10,
+      ceramics: 10,
+      bricks: 10,
+      limestone: 8,
+      marble: 8,
+      luxury_furniture: 6,
+      bronze: 5,
+      glass: 5,
+      paper: 5,
+      ink: 4,
+      money: 20000
+    },
+    maintenance: {
+      money: 5000,
+      machine_parts: 8,
+      common_furniture: 5,
+      electric_gear: 6,
+      computers: 3,
+      telephones: 2
+    },
+    manpower_cost: {
+      any_pop: {
+        labourers: 10000,
+        peasants: 10000
+      }
+    },
+    modifiers: {
+      enable_companies: true,
+      enable_stock_trading: true,
+      administrative_efficiency: 0.02
+    },
+    produces: {
+      money: 20000
+    }
+  }
+};
---
+++ common/buildings/education.js
@@ -0,0 +1,237 @@
+config.buildings.education = { //[WIP] - Add special_effect
+  order: 5,
+
+  art_academies: {
+    name: "Art Academies",
+    singular: "Art Academy",
+
+    construction_turns: 5,
+    cost: {
+      bricks: 20,
+      glass: 12,
+      stone: 12,
+      limestone: 10,
+      lumber: 10,
+      dyes: 8,
+      common_furniture: 5,
+      granite: 5,
+      gold: 3,
+      money: 15000
+    },
+    maintenance: {
+      money: 1500,
+      bricks: 20,
+      limestone: 10,
+      glass: 8,
+      lamps: 6,
+      marble: 5,
+      paper: 4,
+      dyes: 3,
+      ink: 3,
+      common_furniture: 2,
+      computers: 2,
+      gold: 2,
+      tools: 2
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 2500,
+        faculty: 2500
+      }
+    },
+    modifiers: {
+      cultural_research_efficiency: 0.20,
+      artisan_pop_chance: 0.02
+    }
+  },
+  community_colleges: {
+    name: "Community Colleges",
+    singular: "Community College",
+    aliases: ["cc"],
+
+    construction_turns: 6,
+    cost: {
+      bricks: 25,
+      iron: 20,
+      steel_beams: 15,
+      glass: 13,
+      lumber: 10,
+      machine_parts: 5,
+      common_furniture: 5,
+      slate: 5,
+      chalk: 3,
+      money: 9000
+    },
+    maintenance: {
+      money: 1000,
+      lamps: 5,
+      paper: 5,
+      ink: 4,
+      computers: 3,
+      typewriters: 2,
+      chalk: 1,
+      slate: 1,
+      telephones: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 3500,
+        faculty: 3500
+      },
+      artisans: 1000
+    }
+  },
+  libraries: {
+    name: "Libraries",
+    singular: "Library",
+
+    construction_turns: 4,
+    cost: {
+      lumber: 15,
+      paper: 12,
+      stone: 8,
+      glass: 5,
+      common_furniture: 5,
+      textiles: 3,
+      money: 3500
+    },
+    maintenance: {
+      money: 500,
+      paper: 6,
+      common_furniture: 4
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 5000,
+        faculty: 5000
+      }
+    }
+  },
+  primary_schools: {
+    name: "Primary Schools",
+    singular: "Primary School",
+
+    construction_turns: 3,
+    cost: {
+      bricks: 10,
+      lumber: 8,
+      glass: 5,
+      common_furniture: 5,
+      glass: 3,
+      slate: 2,
+      money: 3000
+    },
+    maintenance: {
+      money: 300,
+      paper: 3,
+      ink: 3,
+      slate: 2,
+      chalk: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 1000,
+        faculty: 1000
+      }
+    }
+  },
+  schoolhouses: {
+    name: "Schoolhouses",
+    singular: "Schoolhouse",
+
+    construction_turns: 2,
+    cost: {
+      lumber: 8,
+      wood: 3,
+      common_furniture: 2,
+      slate: 2,
+      glass: 1,
+      money: 500
+    },
+    maintenance: {
+      money: 100,
+      paper: 1,
+      slate: 1,
+      chalk: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 100,
+        faculty: 100
+      }
+    }
+  },
+  secondary_schools: {
+    name: "Secondary Schools",
+    singular: "Secondary School",
+
+    construction_turns: 5,
+    cost: {
+      bricks: 15,
+      stone: 12,
+      lumber: 10,
+      paper: 8,
+      wood: 5,
+      common_furniture: 5,
+      glass: 5,
+      slate: 2,
+      money: 8500
+    },
+    maintenance: {
+      money: 800,
+      paper: 2,
+      ink: 2,
+      slate: 2,
+      chalk: 1,
+      computers: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 5000,
+        faculty: 5000
+      }
+    }
+  },
+  universities: {
+    name: "Universities",
+    singular: "University",
+
+    construction_turns: 8,
+    cost: {
+      bricks: 20,
+      iron: 15,
+      stone: 15,
+      glass: 8,
+      paper: 8,
+      common_furniture: 5,
+      ink: 5,
+      slate: 5,
+      slate: 4,
+      chalk: 3,
+      dyes: 2,
+      money: 20000
+    },
+    maintenance: {
+      money: 4000,
+      lamps: 3,
+      foods: 2,
+      ink: 2,
+      paper: 2,
+      slate: 2,
+      chalk: 2,
+      electric_gear: 1,
+      machine_parts: 1
+    },
+    manpower_cost: {
+      any_pop: {
+        scholars: 15000,
+        faculty: 15000
+      },
+      scientists: 1000
+    },
+    produces: {
+      faculty: 2000,
+      scientists: 1000
+    }
+  }
+};
