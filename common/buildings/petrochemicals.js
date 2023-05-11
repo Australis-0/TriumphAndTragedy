9f5061553c9bbfb84832b5dee76a68e95c62a235
Australis <htmlprogrammingbn@gmail.com>
2023-05-11 16:46:19 +0500

    fin'd petrochemical stubs!

--- common/buildings/petrochemicals.js
+++ common/buildings/petrochemicals.js
@@ -1,4 +1,4 @@
-config.buildings.petrochemicals = {
+config.buildings.petrochemicals = { //[WIP] - Add maintenance/production fields
   order: 13,
 
   acetic_acid_plants: {
@@ -313,6 +313,298 @@ config.buildings.petrochemicals = {
       lamps: 3,
       copper_wire: 3,
       money: 25000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  iodine_plants: {
+    name: "Iodine Plants",
+    singular: "Iodine Plant",
+
+    construction_turns: 4,
+    cost: {
+      steel_beams: 8,
+      machine_parts: 6,
+      steel: 6,
+      reinforced_concrete: 4,
+      lamps: 2,
+      money: 20000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  lye_factories: {
+    name: "Lye Factories",
+    singular: "Lye Factory",
+
+    construction_turns: 3,
+    cost: {
+      bricks: 6,
+      steel: 4,
+      cement: 3,
+      machine_parts: 3,
+      lumber: 2,
+      copper: 2,
+      money: 12000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  methanol_plants: {
+    name: "Methanol Plants",
+    singular: "Methanol Plant",
+
+    construction_turns: 4,
+    cost: {
+      stainless_steel: 6,
+      steel: 6,
+      machine_parts: 5,
+      reinforced_concrete: 4,
+      lamps: 3,
+      copper_wire: 3,
+      money: 8500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  mtbe_manufacturing_plants: {
+    name: "MTBE Manufacturing Plants",
+    singular: "MTBE Manufacturing Plant",
+    aliases: ["methyl tertiary-butyl ether", "methyl tert-butyl ether", "tert-butyl methyl ether", "methyl tertiary butyl ether"],
+
+    construction_turns: 5,
+    cost: {
+      steel_beams: 8,
+      stainless_steel: 5,
+      steel: 5,
+      reinforced_concrete: 4,
+      lamps: 3,
+      copper_wire: 3,
+      money: 8500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  natural_gas_extractors: {
+    name: "Natural Gas Extractors",
+    singular: "Natural Gas Extractor",
+
+    construction_turns: 6,
+    cost: {
+      electric_gear: 6,
+      steel_beams: 6,
+      machine_parts: 5,
+      lightbulbs: 4,
+      money: 10000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  nitric_acid_plants: {
+    name: "Nitric Acid Plants",
+    singular: "Nitric Acid Plant",
+
+    construction_turns: 4,
+    cost: {
+      machine_parts: 8,
+      tungsten: 6,
+      glass: 5,
+      stainless_steel: 5,
+      steel_beams: 5,
+      lamps: 2,
+      money: 12500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  oxygen_chemical_factories: {
+    name: "Oxygen Chemical Factories",
+    singular: "Oxygen Chemical Factory",
+    aliases: ["oxygen plants", "oxygen factories"],
+
+    construction_turns: 5,
+    cost: {
+      machine_parts: 7,
+      reinforced_concrete: 6,
+      steel: 6,
+      aluminium: 4,
+      electric_gear: 3,
+      money: 15000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  phenol_plants: {
+    name: "Phenol Plants",
+    singular: "Phenol Plant",
+
+    construction_turns: 4,
+    cost: {
+      machine_parts: 12,
+      bricks: 10,
+      cement: 8,
+      steel_beams: 8,
+      copper: 7,
+      lead: 5,
+      lumber: 5,
+      lamps: 4,
+      money: 8500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  phosphate_works: {
+    name: "Phosphate Works",
+    singular: "Phosphate Works",
+
+    construction_turns: 4,
+    cost: {
+      machine_parts: 8,
+      lumber: 8,
+      steel_beams: 5,
+      glass: 5,
+      concrete: 3,
+      money: 10000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  plastic_factories: {
+    name: "Plastic Factories",
+    singular: "Plastic Factory",
+    aliases: ["plastic works"],
+
+    construction_turns: 3,
+    cost: {
+      machine_parts: 12,
+      reinforced_concrete: 10,
+      electric_gear: 8,
+      alumina: 6,
+      copper: 4,
+      lightbulbs: 3,
+      money: 15000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  refineries: {
+    name: "Refineries",
+    singular: "Refinery",
+
+    construction_turns: 6,
+    cost: {
+      iron: 15,
+      steel_beams: 12,
+      machine_parts: 8,
+      concrete: 8,
+      copper: 6,
+      copper_wire: 5,
+      lumber: 4,
+      glass: 3,
+      money: 8800
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  sodium_carbonate_works: {
+    name: "Sodium Carbonate Works",
+    singular: "Sodium Carbonate Works",
+
+    construction_turns: 6,
+    cost: {
+      bricks: 12,
+      machine_parts: 10,
+      iron: 9,
+      glass: 8,
+      cement: 8,
+      lamps: 6,
+      money: 12500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  steam_crackers: {
+    name: "Steam Crackers",
+    singular: "Steam Cracker",
+
+    construction_turns: 2,
+    cost: {
+      steel_beams: 10,
+      machine_parts: 10,
+      iron: 9,
+      glass: 8,
+      cement: 8,
+      lamps: 6,
+      money: 12500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  titanium_dioxide_works: {
+    name: "Titanium Dioxide Works",
+    singular: "Titanium Dioxide Works",
+
+    construction_turns: 4,
+    cost: {
+      machine_parts: 8,
+      reinforced_concrete: 6,
+      electric_gear: 5,
+      aluminium: 5,
+      glass: 4,
+      lightbulbs: 3,
+      money: 15000
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  toluene_works: {
+    name: "Toluene Works",
+    singular: "Toluene Works",
+
+    construction_turns: 3,
+    cost: {
+      iron: 12,
+      steel_beams: 10,
+      machine_parts: 8,
+      bricks: 8,
+      glass: 5,
+      copper_wire: 4,
+      lightbulbs: 3,
+      cement: 2,
+      money: 12500
+    },
+    manpower_cost: {
+      engineers: 10000
+    }
+  },
+  urea_factories: {
+    name: "Urea Factories",
+    singular: "Urea Factory",
+
+    construction_turns: 4,
+    cost: {
+      steel: 9,
+      steel_beams: 8,
+      machine_parts: 8,
+      lumber: 5,
+      lightbulbs: 4,
+      cement: 4,
+      money: 15000
     },
     manpower_cost: {
       engineers: 10000
