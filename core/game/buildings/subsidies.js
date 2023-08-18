c5dd851be05b3aa22c9d01a6664070334967c4d1
Australis <htmlprogrammingbn@gmail.com>
2023-08-18 13:37:16 +0500

    added subsidiseAllBuildings() & subsidiseBuilding()!

---
+++ core/game/buildings/subsidies.js
@@ -0,0 +1,100 @@
+module.exports = {
+  /*
+    subsidiseAllBuildings() - Subsidises all of a user's buildings. Fires once per turn if user has toggleAllSubsidies() enabled.
+    options: {
+      desubsidise: true/false, - Turn off subsidies for all buildings instead of subsidisng
+      province_ids: [] - The province IDs to target for subsidies instead of targeting a user's global provinces
+    }
+  */
+  subsidiseAllBuildings: function (arg0_user, arg1_options) {
+    //Convert from parameters
+    var user_id = arg0_user;
+    var options = (arg1_options) ? arg1_options : {};
+
+    //Declare local instance variables
+    var actual_id = main.global.user_map[user_id];
+    var all_provinces;
+    var game_obj = getGameObject(user_id);
+    var total_subsidised = 0;
+    var usr = main.users[actual_id];
+
+    //Handle options.province_ids
+    if (options.province_ids) {
+      all_provinces = [];
+      options.province_ids = getList(options.province_ids);
+
+      for (var i = 0; i < options.province_ids.length; i++)
+        all_provinces.push(main.provinces[options.province_ids[i]]);
+    } else {
+      all_provinces = getProvinces(user_id);
+    }
+
+    //Set all buildings to being subsidised
+    for (var i = 0; i < all_provinces.length; i++)
+      if (all_provinces[i].buildings)
+        for (var x = 0; x < all_provinces[i].buildings.length; x++) {
+          var local_building = all_provinces[i].buildings[x];
+
+          //Check if building is being subdisied/desubsidised
+          if (local_building.subsidised) {
+            if (options.desubsidise) {
+              total_subsidised++;
+              delete local_building.subsidised;
+            }
+          } else {
+            if (!options.desubsidise) {
+              total_subsidised++;
+              local_building.subsidised = true;
+            }
+          }
+        }
+
+    //Return statement
+    return total_subsidised;
+  },
+
+  /*
+    subsidiseBuilding() - Subsidises an individual building or turns off subsidies.
+    options: {
+      desubsidise: true/false, - Turn off subsidies instead of subsidising the building
+
+      do_not_display: true/false - Whether to display the action in the player's UI
+    }
+  */
+  subsidiseBuilding: function (arg0_user, arg1_building, arg2_options) {
+    //Convert from parameters
+    var user_id = arg0_user;
+    var building_obj = arg1_building;
+    var options = (arg2_options) ? arg2_options : {};
+
+    //Declare local instance variables
+    var actual_id = main.global.user_map[user_id];
+    var game_obj = getGameObject(user_id);
+    var province_id = building_obj.split("-")[0];
+    var province_obj = main.provinces[province_id];
+    var usr = main.users[actual_id];
+
+    //Subsidise building
+    if (province_obj.controller == actual_id) {
+      if (!building_obj.insolvent) {
+        if (!options.desubsidised) {
+          delete building_obj.subsidised;
+
+          if (!options.do_not_display)
+            printAlert(game_obj.id, `We have stopped subsidies for **${(building_obj.name) ? building_obj.name : building_obj.id}**.`);
+        } else {
+          building_obj.subsidised = true;
+
+          if (!options.do_not_display)
+            printAlert(game_obj.id, `We have subsidised the **${(building_obj.name) ? building_obj.name : building_obj.id}**.`);
+        }
+      } else {
+        if (!options.do_not_display)
+          printError(game_obj.id, `We must manually reopen this building first for ${config.icons.money} **${parseNumber(getReopenCost(building_obj))}** by typing **[Reopen Building]**!`);
+      }
+    } else {
+      if (!options.do_not_display)
+        printError(game_obj.id, `You must control **${(province_obj.name) ? province_obj.name : `Province ${province_obj.id}`} before being able to control subsidies there!`);
+    }
+  }
+};
