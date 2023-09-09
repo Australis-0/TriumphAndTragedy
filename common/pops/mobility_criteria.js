01c045ce9555b22098e2a5887f7ffa0278680f09
Australis <htmlprogrammingbn@gmail.com>
2023-09-09 18:29:53 +0500

    fin'd general promotion/demotion configs!

--- common/pops/mobility_criteria.js
+++ common/pops/mobility_criteria.js
@@ -40,7 +40,6 @@ config.pop_mobility = {
       value: 0.005
     },
     per_percent_promotion_chance: {
-      max: 0,
       value: 0.005,
 
       promotion_chance: -0.10
@@ -64,7 +63,7 @@ config.pop_mobility = {
       value: -0.02
     }
   },
-  promotion: { //[WIP] - Promotion scope
+  promotion: {
     limit: {
       has_staple_goods: true,
       has_staple_goods_variety: 0.40,
@@ -73,6 +72,86 @@ config.pop_mobility = {
       imprisoned: false,
       homeless: false,
       occupied: false
+    },
+    add_chance_luxury_goods: {
+      limit: {
+        has_luxury_goods: true,
+        has_luxury_goods_variety: 0.20
+      },
+      value: 0.01
+    },
+    add_chance_luxury_goods_2: {
+      limit: {
+        has_luxury_goods: true,
+        has_luxury_goods_variety: 0.50
+      },
+      value: 0.005
+    },
+    per_wealth: {
+      max: 0.25,
+
+      wealth: 10000,
+      value: 0.005
+    },
+    per_percent_education_level: {
+      education_level: 0.10,
+      value: 0.002
+    },
+    per_percent_enslaved_pops: {
+      enslaved: 0.02,
+      value: -0.001
+    },
+    add_chance_militancy: {
+      militancy: 5,
+      value: -0.005
+    },
+    add_chance_militancy_2: {
+      militancy: 10,
+      value: -0.005
+    },
+    add_chance_mental_health_1: {
+      mental_health_less_than: 0.20,
+      value: -0.005
+    },
+    per_percent_discrimination: {
+      discrimination: 0.10,
+      value: -0.005
+    },
+    add_chance_happiness: {
+      happiness_less_than: 0.30,
+      value: -0.01
+    },
+    add_chance_mental_health_2: {
+      mental_health_less_than: 0.50,
+      value: -0.01
+    },
+    per_percent_crime: {
+      crime: 0.05,
+      value: -0.01
+    },
+    add_chance_not_accepted: {
+      limit: {
+        has_accepted_culture: false
+      },
+      value: -0.025
+    },
+    add_chance_education_03: {
+      limit: {
+        education_level_less_than: 0.20
+      },
+      value: -0.04
+    },
+    add_chance_mental_health_3: {
+      limit: {
+        mental_health_less_than: 0.10
+      },
+      value: -0.05
+    },
+    add_chance_no_jobs: {
+      limit: {
+        no_jobs: true
+      },
+      value: -0.05
     }
   }
 };
